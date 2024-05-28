2d1508a03d8e22b59dc5723b111db1c1a221fecf
abr-Projects <abfreelance1999@gmail.com>
2024-05-29 01:40:50 +0800

    second

--- src/client.ts
+++ src/client.ts
@@ -1,40 +1,265 @@
+
 import "./styles.css";
 
 import PartySocket from "partysocket";
 
 declare const PARTYKIT_HOST: string;
 
-let pingInterval: ReturnType<typeof setInterval>;
+const problems = ["General IT","No Sound","Cannot Open Files","Projector Failure","Monitors no display","Others"]
 
-// Let's append all the messages we get into this DOM element
-const output = document.getElementById("app") as HTMLDivElement;
 
-// Helper function to add a new line to the DOM
-function add(text: string) {
-  output.appendChild(document.createTextNode(text));
-  output.appendChild(document.createElement("br"));
+const ws = new PartySocket({
+  host: PARTYKIT_HOST,
+  room: "global",
+  id:  localStorage.getItem("conn_id") !
+ 
+
+});
+
+
+function preventInput(e:any){
+  const inputField = <HTMLInputElement>e.target
+  let inputValue = parseInt(inputField.value);
+  inputValue = isNaN(inputValue) ? parseInt(e.key) : parseInt(inputValue.toString()+e.key)
+
+  if (inputValue > parseInt(inputField.max) || inputValue < parseInt(inputField.min)){
+      e.preventDefault();
+
+    
+  }
 }
 
-// A PartySocket is like a WebSocket, except it's a bit more magical.
-// It handles reconnection logic, buffering messages while it's offline, and more.
-const conn = new PartySocket({
-  host: PARTYKIT_HOST,
-  room: "my-new-room",
+
+
+window.document.addEventListener("DOMContentLoaded",function(){
+
+ 
+  
+  if (window.document.location.pathname == '/'){
+    window.document.getElementById("login")!.onclick = async function() {
+
+      const id = (<HTMLInputElement>document.getElementById("staffId")).value
+      const password = (<HTMLInputElement>document.getElementById("password")).value
+    
+        PartySocket.fetch(
+          {
+            host: PARTYKIT_HOST,
+            room: "global",
+          },
+          {
+    
+            method: "POST",
+            body: JSON.stringify({ type: "login", id: id, password: password }),
+          }
+        ) .then(function(res) {
+          if(!res.ok){
+            console.log("YOU ARE NOT OK!")
+          }
+          return res.json();
+        }).then(function(data : any) {
+    
+          localStorage.setItem("Token", data.token);
+          localStorage.setItem("Access", data.access);
+          localStorage.setItem("Name",data.name);
+          if (localStorage.getItem("conn_id") === null){
+            localStorage.setItem('conn_id',localStorage.getItem("Access")!+Date.now())
+          }
+          if (localStorage.getItem("Access") === "2"){
+            window.location.assign('/support.html')
+
+          }
+          else if(localStorage.getItem("Access") === "3"){
+            window.location.assign('/ticket.html')
+
+          }
+        })    
+    };
+  }
+  else if(window.document.location.pathname == '/ticket.html'){
+    if (localStorage.getItem("Access") === null){
+      window.location.assign('/')
+    }
+    ws.send(JSON.stringify({type:"ready",access:localStorage.getItem("Access")}))
+    
+    problems.forEach((el) => {
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.id = el.trim();
+      checkbox.value = el;
+      checkbox.name = "problems"
+
+  
+      const label = document.createElement('label');
+      label.htmlFor = el.trim();
+      label.textContent = " " + el + " ";
+
+      window.document.getElementById("problems")!.appendChild(checkbox)
+
+      window.document.getElementById("problems")!.appendChild(label)
+
+    })
+    window.document.getElementById("floor")!.addEventListener("keydown", preventInput)
+    window.document.getElementById("room")!.addEventListener("keydown", preventInput)
+    window.document.getElementById("submit")!.onclick = async function() {
+      console.log("SUBMITED")
+      document.getElementById("status")!.innerHTML = "IT FAILURE (PENDING)"
+      const floor = (<HTMLInputElement>document.getElementById("floor")).value
+      const room = (<HTMLInputElement>document.getElementById("room")).value
+      const desc = (<HTMLTextAreaElement>document.getElementById("description")).value
+      let selectedProblems: string[] = [];
+      let checkboxes = document.getElementsByName('problems') as NodeListOf<HTMLInputElement>;
+      checkboxes.forEach((el: HTMLInputElement) => {
+        if (el.checked) {
+          selectedProblems.push(el.value);
+        }
+      });
+
+      const ticket = {
+        type:"ticket",
+        req_id: Date.now(),
+        room: floor + room, 
+        problems: selectedProblems,
+        desc: desc,
+        name:localStorage.getItem("Name"),
+        auth: localStorage.getItem("Token")
+        
+      };
+      console.log(ticket)
+      ws.send(JSON.stringify(ticket))
+
+    
+    }
+  }
+  else if(window.document.location.pathname == '/support.html'){
+    if (localStorage.getItem("Access") === null){
+      window.location.assign('/')
+    }
+    PartySocket.fetch(
+          {
+            host: PARTYKIT_HOST,
+            room: "global",
+          },
+          {
+    
+            method: "POST",
+            body: JSON.stringify({ type: "tickets",auth:localStorage.getItem("Token")}),
+          }
+        ) .then(function(res) {
+          if(!res.ok){
+            console.log("YOU ARE NOT OK!")
+          }
+          return res.json();
+        }).then(function(data : any) {
+
+          console.log(data)
+          data.forEach((el : any) => {
+            const row = document.createElement("tr");
+
+
+            const req_id = document.createElement("td");
+            req_id.textContent = el.request_id;
+            row.appendChild(req_id);
+
+            const name = document.createElement("td");
+            name.textContent = el.name
+            row.appendChild(name);
+
+            const room = document.createElement("td");
+            room.textContent = el.classroom
+            row.appendChild(room);
+
+            const desc = document.createElement("td");
+            desc.textContent = el.dsc
+            row.appendChild(desc);
+
+            const problems = document.createElement("td");
+            problems.textContent = el.issues
+            row.appendChild(problems);
+
+            const accept = document.createElement("td");
+            const aBtn = document.createElement("button");
+            aBtn.textContent = "Accept";
+            aBtn.addEventListener("click", () => {
+              const acceptMessage  = {
+                type:"accept",
+                name:localStorage.getItem("Name"),
+                req_id: el.request_id
+              }
+              ws.send(JSON.stringify(acceptMessage))
+              row.remove()
+              
+            });
+            accept.appendChild(aBtn);
+            
+            row.appendChild(accept);
+
+            const table = document.getElementById("tickets")!;
+            table.appendChild(row);
+            
+          });
+        })
+    
+  }
+})
+
+ws.addEventListener("message", (message : any) => {
+  if(window.document.location.pathname == '/support.html'){
+    const ticket = JSON.parse(message.data)
+    const row = document.createElement("tr");
+
+
+    const req_id = document.createElement("td");
+    req_id.textContent = ticket.req_id;
+    row.appendChild(req_id);
+
+    const name = document.createElement("td");
+    name.textContent = ticket.name;
+    row.appendChild(name);
+
+    const room = document.createElement("td");
+    room.textContent = ticket.room;
+    row.appendChild(room);
+
+    const desc = document.createElement("td");
+    desc.textContent = ticket.desc;
+    row.appendChild(desc);
+
+    const problems = document.createElement("td");
+    problems.textContent = ticket.problems.join(", ");
+    row.appendChild(problems);
+
+    const accept = document.createElement("td");
+    const aBtn = document.createElement("button");
+    aBtn.textContent = "Accept";
+    aBtn.addEventListener("click", () => {
+      const acceptMessage  = {
+        type:"accept",
+        name:localStorage.getItem("Name"),
+        req_id: ticket.req_id
+      }
+      ws.send(JSON.stringify(acceptMessage))
+      row.remove();
+      
+      
+    });
+    accept.appendChild(aBtn);
+    
+    row.appendChild(accept);
+
+    const table = document.getElementById("tickets")!;
+    table.appendChild(row);
+
+
+
+  }
+  if(window.document.location.pathname == '/ticket.html'){
+    const acceptMessage = JSON.parse(message.data)
+    if (acceptMessage.type == "accepted"){
+      document.getElementById("status")!.innerHTML = `IT FAILURE (ACCEPTED BY ${acceptMessage.name})`
+
+    }
+  }
+  
+
 });
 
-// You can even start sending messages before the connection is open!
-conn.addEventListener("message", (event) => {
-  add(`Received -> ${event.data}`);
-});
-
-// Let's listen for when the connection opens
-// And send a ping every 2 seconds right after
-conn.addEventListener("open", () => {
-  add("Connected!");
-  add("Sending a ping every 2 seconds...");
-  // TODO: make this more interesting / nice
-  clearInterval(pingInterval);
-  pingInterval = setInterval(() => {
-    conn.send("ping");
-  }, 1000);
-});
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,124 @@
 import type * as Party from "partykit/server";
+import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from 'drizzle-orm' 
+import postgres from "postgres";
+import jwt from '@tsndr/cloudflare-worker-jwt'
 
+
+const connectionString = process.env.DB as string;
+const SECRET = process.env.SECRET as string
+const client = postgres(connectionString, { prepare: false });
+const db = drizzle(client);
 export default class Server implements Party.Server {
   constructor(readonly room: Party.Room) {}
 
-  onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
-    // A websocket just connected!
-    console.log(
-      `Connected:
-  id: ${conn.id}
-  room: ${this.room.id}
-  url: ${new URL(ctx.request.url).pathname}`
-    );
+  async onMessage(message: string, sender: Party.Connection) {
+    const req = await JSON.parse(message)
+    if (req.type == "ready"){
+      if(this.room.storage.get(sender.id) != null){
+        this.room.storage.put(sender.id,req.access)
+      }
 
-    // let's send a message to the connection
-    conn.send("hello from server");
+      
+    }
+    if(req.type == "accept"){
+      const data : any = await db.execute(sql`SELECT conn_id from request WHERE request_id = '${sql.raw(req.req_id)}'`)
+      console.log(data)
+      await db.execute(sql`UPDATE request SET rStatus = 'ACCEPTED' WHERE request_id = '${sql.raw(req.req_id)}'`)
+      for (const conn of this.room.getConnections()) {
+        if (conn.id.toString() == data[0].conn_id.toString()) {
+          const acceptMessage = {
+            type:'accepted',
+            name:req.name
+          }
+          conn.send(JSON.stringify(acceptMessage));
+        }
+      }
+
+    }
+    if (req.type == "ticket"){
+      console.log("ticked received")
+      if(await jwt.verify(req.auth,SECRET)){
+
+        const info : any = await jwt.decode(req.auth)
+        console.log(info)
+        await db.execute(sql`INSERT INTO request (request_id, staff_id, technician_id, dsc, rStatus, classroom, conn_id) 
+        VALUES ('${sql.raw(req.req_id)}', '${sql.raw(info.payload.id.toString())}', 0, '${sql.raw(req.desc)}', 'PENDING', '${sql.raw(req.room)}', '${sql.raw(sender.id)}')`);
+          await Promise.all(req.problems.map(async (element: string, index: number) => {
+            console.log(element);
+            await db.execute(sql`INSERT INTO issues (issue_id,request_id, issue) VALUES ('${sql.raw(index+Date.now().toString())}','${sql.raw(req.req_id)}', '${sql.raw(element)}')`);
+        }));
+
+        
+        const onlyTechnicians = Array.from(await this.room.storage.list()).filter(([key, value]) => value != '2').map(([key]) => key);
+        const rK: string = 'auth';
+
+        const sTicket = {...req}
+        delete sTicket[rK];
+
+        this.room.broadcast(JSON.stringify(sTicket),onlyTechnicians)
+
+
+      }
+    }
+   
+
   }
 
-  onMessage(message: string, sender: Party.Connection) {
-    // let's log the message
-    console.log(`connection ${sender.id} sent message: ${message}`);
-    // as well as broadcast it to all the other connections in the room...
-    this.room.broadcast(
-      `${sender.id}: ${message}`,
-      // ...except for the connection it came from
-      [sender.id]
-    );
+  async onRequest(req: Party.Request){
+
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    };
+
+    if (req.method === "OPTIONS") {
+      return new Response(null, { headers });
+    }
+
+    
+    
+    interface LoginRequest {
+      type: String;
+      id : Number;
+      password:String
+    }
+    
+    if(req.method == "POST") {
+      const r = await req.json() as LoginRequest;
+      if(r.type == "login") {
+        const data = await db.execute(sql`SELECT passw , occupation,first_name,last_name FROM useracc u, staff s WHERE u.u_id = s.id and u_id = ${r.id}`)
+
+        if(data.length == 0) {
+          return new Response(JSON.stringify({state:'User not found'}), { status: 404, headers });
+        } else if (r.password != data[0].passw) {
+          return new Response(JSON.stringify({state:'Invalid credentials'}), { status: 401, headers });
+        } else {
+          const token = await jwt.sign({
+            id: r.id,
+            password: r.password,
+          }, SECRET)
+          const name =  data[0].first_name + " " + data[0].last_name;
+        
+          return new Response(JSON.stringify({state:'Access granted',token:token,name:name,access:data[0].occupation}), { status: 200, headers });
+        }
+      }
+      else if(r.type == "tickets"){
+        const data = await db.execute(sql`SELECT r.request_id, r.staff_id, r.dsc, r.classroom, concat(s.first_name,' ',s.last_name) as name , string_agg(i.issue, ', ') AS issues
+        FROM request r
+        LEFT JOIN issues i ON r.request_id = i.request_id
+        LEFT JOIN staff s ON r.staff_id = s.id
+        WHERE r.rStatus = 'PENDING'
+        GROUP BY r.request_id, r.staff_id, s.first_name, s.last_name, r.dsc, r.classroom`)
+   
+        return new Response(JSON.stringify(data), { status: 200, headers });
+
+      }
+    }
+    
+    return new Response('ERR', { status: 404, headers });
   }
 }
 
